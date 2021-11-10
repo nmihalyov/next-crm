06d659d29745fba4e1b56a681f9b768de075f3c7
Nikita Mihalyov <nikita.mihalyov@gmail.com>
2021-11-10 11:46:17 +0300

    Resetup test environment for Enzyme

--- jest.config.js
+++ jest.config.js
@@ -1,25 +1,30 @@
 module.exports = {
+  verbose: true,
+  clearMocks: true,
+  collectCoverage: true,
   collectCoverageFrom: [
     '**/*.{js,jsx,ts,tsx}',
     '!.next/**',
     '!coverage/**',
     '!**/*.d.ts',
-    '!**/node_modules/**',
+    '!**/node_modules/**'
   ],
   moduleNameMapper: {
     '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
     '^.+\\.(css|sass|scss)$': '<rootDir>/__mocks__/styleMock.js',
-    '^.+\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/__mocks__/fileMock.js',
+    '^.+\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/__mocks__/fileMock.js'
   },
   testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/.next/'],
   testEnvironment: 'jsdom',
   transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
+    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', {presets: ['next/babel']}]
   },
+  snapshotSerializers: [
+    './node_modules/enzyme-to-json/serializer'
+  ],
   transformIgnorePatterns: [
     '/node_modules/',
-    '^.+\\.module\\.(css|sass|scss)$',
+    '^.+\\.module\\.(css|sass|scss)$'
   ],
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  testEnvironment: "jsdom"
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js']
 }
--- jest.setup.js
+++ jest.setup.js
@@ -1,15 +1,16 @@
-import '@testing-library/jest-dom/extend-expect';
+import Enzyme, { shallow, render, mount } from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import toJson from 'enzyme-to-json';
 
-Object.defineProperty(window, "matchMedia", {
-  writable: true,
-  value: jest.fn().mockImplementation(query => ({
-    matches: false,
-    media: query,
-    onchange: null,
-    addListener: jest.fn(), // Deprecated
-    removeListener: jest.fn(), // Deprecated
-    addEventListener: jest.fn(),
-    removeEventListener: jest.fn(),
-    dispatchEvent: jest.fn(),
-  }))
+Enzyme.configure({
+  adapter: new Adapter()
 });
+
+global.shallow = shallow;
+global.render = render;
+global.mount = mount;
+global.toJson = toJson;
+
+console.error = message => {
+  throw new Error(message);
+};
